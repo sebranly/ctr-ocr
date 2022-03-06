37f1255804d1179de7427e89cf2b7f2c9995b6a9
sebranly <sebranly.pinf@gmail.com>
2022-03-06 17:52:05 +0100

    Updates

---
+++ precache-manifest.41d36928b49fa2ccfeb4fe93e88dcfdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b4a561a19e23cd3154aa9f5ad0f7979",
+    "url": "/ctr-ocr/index.html"
+  },
+  {
+    "revision": "0a20324e9f87f5d3c0f6",
+    "url": "/ctr-ocr/static/css/main.4096521b.chunk.css"
+  },
+  {
+    "revision": "97fb5afa5100b33a46b4",
+    "url": "/ctr-ocr/static/js/2.21e40266.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ctr-ocr/static/js/2.21e40266.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a20324e9f87f5d3c0f6",
+    "url": "/ctr-ocr/static/js/main.a0b534dd.chunk.js"
+  },
+  {
+    "revision": "777cb74ae909bd2504ea",
+    "url": "/ctr-ocr/static/js/runtime-main.41876357.js"
+  }
+]);
