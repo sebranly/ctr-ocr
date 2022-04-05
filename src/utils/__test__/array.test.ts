5ae411eb4404e10e819971990a82c41b9b3ebe11
sebranly <sebranly.pinf@gmail.com>
2022-04-06 00:01:08 +0200

    feat: add self-review

--- src/utils/__test__/array.test.ts
+++ src/utils/__test__/array.test.ts
@@ -7,4 +7,5 @@ test('isEqual', () => {
   expect(isEqual(['bonjour', 'bonsoir', 'rebonsoir'], ['bonjour', 'bonsoir', 'rebonsoir'])).toBe(true);
   expect(isEqual(['bonjour'], [1])).toBe(false);
   expect(isEqual([1, 2], [2, 1])).toBe(false);
+  expect(isEqual(['bonjour'], ['bonjour', 'bonsoir'])).toBe(false);
 });
